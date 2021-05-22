bf452c2771e7833a4ff9f2dd2c1a3342bc8ffee4
Mahakeerthi <mahakeerthi3@gmail.com>
2021-05-22 21:47:18 +0530

    Project setup is complete.

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <h1 className="text-blue-900">
+        Best Selling Cars in india project setup is complete
+      </h1>
+    </Fragment>
+  );
+}
